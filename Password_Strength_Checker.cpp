6f3984724ff87618a091396e7c4dc0fc892fdc33
Tyce Webster <tycewebster@gmail.com>
2025-02-27 14:35:58 -0600

    Updated Password_Strength_Checker.cpp
    Added the printStrongness function to the file and included the needed variables

--- Password_Strength_Checker.cpp
+++ Password_Strength_Checker.cpp
@@ -1,2 +1,15 @@
 #include "Password_Project.h"
 
+void printStrongness(string &input)
+{
+    int n = input.length();
+
+    bool hasLower = false;
+    bool hasUpper = false;
+    bool hasDigit = false;
+    bool hasSpecial = false;
+
+    string normalChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+    
+}
+
